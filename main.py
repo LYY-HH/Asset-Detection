312bb21f27cf895602c103e26fc0def892496d5f
wangyx <2789582528@qq.com>
2021-06-09 02:43:24 +0800

    添加了请求方式判断和注释

--- main.py
+++ main.py
@@ -13,26 +13,31 @@ my_col.delete_many({})
 # 创建任务
 @app.route('/api/create_task', methods=['POST'])
 def create_task():
-    datajson = {
-        'name': request.json.get('task_name'),
-        'description': request.json.get('task_desc'),
-        'ip': request.json.get('task_ip'),
-        'port': request.json.get('task_port'),
-        'thread_num': request.json.get('task_thread_num')
-    }
+    if request.method == 'POST':
+        #  缺少参数校验，方法一使用 flask_restful 中的 reqparse;
+        #  方法二，使用 WTforms，参考链接: https://zhuanlan.zhihu.com/p/336091060
+        #                                                                       ---wangyx
+        datajson = {
+            'name': request.json.get('task_name'),
+            'description': request.json.get('task_desc'),
+            'ip': request.json.get('task_ip'),
+            'port': request.json.get('task_port'),
+            'thread_num': request.json.get('task_thread_num')
+        }
 
-    # 将数据存到数据库并获取对应数据的task_id
-    result = my_col.insert_one(datajson)
-    task_id = result.inserted_id
-    datajson['task_id'] = task_id
-    # 将任务加入到celery任务队列中 输出参数为 task_id
-    work.delay(str(task_id))
+        # 将数据存到数据库并获取对应数据的task_id
+        result = my_col.insert_one(datajson)
+        task_id = result.inserted_id
+        datajson['task_id'] = task_id
+        # 将任务加入到celery任务队列中 输出参数为 task_id
+        work.delay(str(task_id))
 
 
 # 获取所有任务队列
 @app.route('/api/task/list', methods=['GET'])
 def task_list():
-    return json_util.dumps(my_col.find())
+    if request.method == 'GET':
+        return json_util.dumps(my_col.find())
 
 
 if __name__ == "__main__":
